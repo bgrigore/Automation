1b6a34fd25049c1d2dc9ef9e6b43b75b91d353dc
bgrigore <bgrigore@users.noreply.github.com>
2015-08-03 17:11:21 +0300

    Update GmailTest.java

--- src/test/java/com/endava/GmailTest.java
+++ src/test/java/com/endava/GmailTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by bgrigore on 8/3/2015.
  */
@@ -19,6 +21,7 @@ public class GmailTest {
     @BeforeClass
     public static void before(){
         driver= new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
     @AfterClass
     public static void after(){
@@ -33,9 +36,25 @@ public class GmailTest {
     @Test
     public void testGmail(){
         WebElement userNameField = driver.findElement(By.id("Email"));
-        userNameField.sendKeys("antonia.im.jobs@gmail.com");
+        userNameField.sendKeys("tt88252@gmail.com");
 
         WebElement nextButton = driver.findElement(By.id("next"));
         nextButton.click();
+
+        WebElement passwordNameField = driver.findElement(By.id("Passwd"));
+        passwordNameField.sendKeys("1234qazws");
+
+        WebElement signInButton = driver.findElement(By.id("signIn"));
+        signInButton.click();
+
+        WebElement composeButton = driver.findElement(By.xpath(".//*[@id=':4i']/div/div"));
+        composeButton.click();
+
+        driver.findElement(By.className("vO")).sendKeys("tt88252@gmail.com");
+        driver.findElement(By.className("aoT")).sendKeys("Test1");
+        driver.findElement(By.className("Am")).sendKeys("Tessst1");
+        driver.findElement(By.xpath("//div[text()='Send']")).click();
+
+
     }
 }
